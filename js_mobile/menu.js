9281d2409f3047da818b043307aad5a107286bff
Jihyun Jeon <hyuneejj@naver.com>
2024-08-26 15:10:04 +0900

    Complete main

---
+++ js_mobile/menu.js
@@ -0,0 +1,11 @@
+const nav = document.getElementById("nav");
+
+// 메뉴 열기
+function openMenu() {
+    nav.style.right = '0';
+}
+
+// 메뉴 닫기
+function closeMenu() {
+    nav.style.right = '-300px';
+}
