7dfc242fc16a8dbddd29302c8d36c36e718f185b
Jihyun Jeon <hyuneejj@naver.com>
2024-08-28 11:10:36 +0900

    fix: PC 스크롤 버튼 수정

--- js_pc/scroll.js
+++ js_pc/scroll.js
@@ -1,17 +1,16 @@
-        // 스크롤 탑 버튼 요소 가져오기
         const scrollTopBtn = document.getElementById('scroll-top-btn');
 
-        // 스크롤 이벤트 리스너 추가
+        // 스크롤 시 나타나게
         window.addEventListener('scroll', function () {
 
-            if (window.scrollY > 100) { // 스크롤이 100px 이상 내려가면 버튼 표시
+            if (window.scrollY > 0) {
                 scrollTopBtn.style.display = "block";
             } else {
                 scrollTopBtn.style.display = "none";
             }
         });
 
-        // 스크롤 탑 함수 정의
+        // 최상단으로 올라가게
         function scrollToTop() {
             window.scrollTo({
                 top: 0,
